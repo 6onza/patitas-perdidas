28cca990b51dade64f62243eac33cfeaf4df24ba
6onza <gvineg@fi.uba.ar>
2024-11-07 16:21:42 -0300

    Added static_url_path to the app config

--- frontend/app/run.py
+++ frontend/app/run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__, template_folder='../templates', static_folder='../static', static_url_path='/static')
 
 @app.route('/')
 def index():
